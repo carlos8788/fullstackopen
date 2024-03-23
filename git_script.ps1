fbe5987c951d1a7e6450fbfc9776cb74253bc596
Luis <100473510+carlos8788@users.noreply.github.com>
2024-03-23 06:59:09 -0300

    git_script

--- git_script.ps1
+++ git_script.ps1
@@ -1,27 +1,20 @@
-# Espera a que el usuario presione Enter para continuar
-# Read-Host "Presiona [Enter] para continuar..."
 
-# Asegúrate de que se pasó exactamente un parámetro al script
 if ($args.Count -ne 1) {
     Write-Host "Uso: $($MyInvocation.MyCommand.Name) <rama_de_desarrollo>"
     exit 1
 }
 
-# Asigna los parámetros a variables para mejorar la legibilidad
 $ramaDesarrollo = $args[0]
 $ramaPrincipal = 'app'
 $fechaActual = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
-# Cambia a la rama principal
 git checkout $ramaPrincipal
 
-# Fusiona la rama de desarrollo en la principal
 git merge $ramaDesarrollo --no-ff -m "Merge $ramaDesarrollo into $ramaPrincipal time: $fechaActual"
 
 git add .
 
 git commit -m "Merge $ramaDesarrollo time: $fechaActual"
-# Hace push de la rama principal al repositorio remoto
 git push origin $ramaPrincipal
 
 git checkout $ramaDesarrollo
