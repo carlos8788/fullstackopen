099300c237aed008f0221b8695fb8bd4fbe80bc2
Luis <100473510+carlos8788@users.noreply.github.com>
2024-04-05 09:08:59 -0300

    6c ready

--- part6/src/App.jsx
+++ part6/src/App.jsx
@@ -1,10 +1,25 @@
-import Notes from './components/Notes'
+import { useEffect } from 'react'
 import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+
+  const styles = {
+    background: '#222',
+    color: 'white',
+    height: '100vh',
+  }
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(initializeNotes()) 
+  }, []) 
+
   return (
-    <div>
+    <div style={styles}>
       <NewNote />
       <VisibilityFilter />
       <Notes />
